a96050398dc16c3c704ad328f721f77524c1bebf
vaibhav malave <vaibhavmalave1994@gmail.com>
2020-01-20 22:16:56 +0530

    created simple example to access flags with error handling and new flag set

--- commandlineflags/main.go
+++ commandlineflags/main.go
@@ -4,15 +4,36 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
+type Person struct {
+	Name   string
+	Age    int
+	IsGood bool
+}
+
+var person Person
+
+func init() {
+	flagSet := flag.NewFlagSet("person", flag.ContinueOnError)
+	var name string
+	var age int
+	var isGood bool
+
+	flagSet.StringVar(&name, "name", "", "name of the person")
+	flagSet.IntVar(&age, "age", 1, "age of the person")
+	flagSet.BoolVar(&isGood, "isgood", false, "is person good")
+
+	err := flagSet.Parse(os.Args[1:])
+	if err != nil {
+		fmt.Println("error while parsing flags.")
+		os.Exit(1)
+	}
+	person = Person{name, age, isGood}
+}
+
 func main() {
-
-	fmt.Println("Program Started here")
-	flag1 := flag.String("name", "self", "name of the person")
-	flag.Parse()
-	fmt.Println(*flag1)
-
-	//to print non flaged arguments
-	fmt.Println("Non flaged arguments", flag.Args())
+	fmt.Println("person", person)
+	fmt.Println("ok")
 }
